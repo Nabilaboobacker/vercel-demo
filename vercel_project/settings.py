793267450dbc5fc9cc10c04cff8637ba38a0da40
Nabilaboobacker <132455030+Nabilaboobacker@users.noreply.github.com>
2024-10-19 19:05:09 +0400

    admin

--- vercel_project/settings.py
+++ vercel_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -87,7 +87,6 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'PORT': config('DB_PORT')
 
-        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
